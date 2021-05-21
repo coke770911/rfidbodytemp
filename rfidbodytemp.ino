63ddb9ab363e1773b7fa899a154ff2a8dcbfa3ec
Joey Xu <coke770911@gmail.com>
2021-05-22 00:36:01 +0800

    完成版
    可顯示體溫及卡號 加上wifi模組可以引入curl 搭配 post web api 存放資料

--- rfidbodytemp.ino
+++ rfidbodytemp.ino
@@ -1,14 +1,14 @@
+#include <Wire.h>
 #include <SPI.h>
 #include <MFRC522.h>
-#include <Wire.h>
 #include <Adafruit_MLX90614.h>
 #include <LiquidCrystal_I2C.h>
 
 #define RST_PIN         9
 #define SS_PIN          10  //就是模組上的SDA接腳
+
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 double Body_temp = 0.00; //宣告個變數 存放體溫
-
 MFRC522 mfrc522;   // 建立MFRC522實體
 LiquidCrystal_I2C lcd(0x27, 20, 4); //建立LED  20字元4列
 
@@ -20,6 +20,8 @@ void setup() {
   Serial.print(F(": "));
   mfrc522.PCD_DumpVersionToSerial(); // 顯示讀卡設備的版本
   mlx.begin();
+  pinMode(7, OUTPUT); //蜂鳴器正極 pin7
+  lcd.init();
 }
 
 void loop() {
@@ -27,24 +29,24 @@ void loop() {
   if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
     // 顯示卡片內容
     lcd.init();                      // initialize the lcd
-    lcd.init();
     lcd.backlight();
 
     lcd.setCursor(0, 0);
-    lcd.print(F("CardUID:"));
-
-    dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size); // 顯示卡片的UID
-
+    lcd.print("CardUID:");
+    lcd.print(dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size));
+    
     MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
-    Serial.println();
     Serial.print(F("PICC type: "));
+    digitalWrite(7, HIGH);
     Serial.println(mfrc522.PICC_GetTypeName(piccType));  //顯示卡片的類型
 
-    //體溫補償
-    Body_temp = (mlx.readAmbientTempC() * 0.16 + mlx.readObjectTempC()) - 1;
+    Body_temp = (mlx.readAmbientTempC() * 0.16 + mlx.readObjectTempC()) - 1; //體溫補償
     lcd.setCursor(0, 1);
-    lcd.print("Body_temp = " + String(Body_temp));
-    mfrc522.PICC_HaltA();  // 卡片進入停止模式
+    lcd.print("Body_temp=" + String(Body_temp));
+    lcd.print((char)0xDF); //符號
+    lcd.print("C"); //符號
+    mfrc522.PICC_HaltA(); // 卡片進入停止模式
+    digitalWrite(7, LOW);
   }
 }
 
@@ -59,12 +61,13 @@ void loop() {
   }
 */
 
-void dump_byte_array(byte *buffer, byte bufferSize) {
+String dump_byte_array(byte *buffer, byte bufferSize) {
   String mystr = "";
   for (byte i = 0; i < bufferSize; i++) {
     mystr = String(buffer[i], HEX) + mystr;
-    
   }
   mystr.toUpperCase();
-  Serial.print(String(mystr));
+  return mystr;
 }
+
+
